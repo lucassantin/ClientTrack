dc9589b2d5a3739f27f10177f74b7996663c2b16
Lucas dos Santos Santin <134106383+lucassantin@users.noreply.github.com>
2025-09-10 15:09:37 -0300

    feat<insight>: add class to refer insight

--- models/insight.py
+++ models/insight.py
@@ -1,3 +1,32 @@
 class Insight:
-    def __init__(self):
-        pass
+    def __init__(self, indice: int, recommendation: str):
+        self.indice = None
+        self.recommendation = None
+
+        def isinstance(indice, int):
+            self.indice = indice
+
+        def isinstance(recommendation, str):
+            self.recommendation = recommendation
+
+    @property
+    def indice(self) -> int:
+        return self._indice
+    
+    @indice.setter
+    def indice(self, indice: int):
+        if isinstance(indice, int):
+            self._indice = indice
+        else:
+            raise TypeError("Indice must be an integer")
+        
+    @property
+    def recommendation(self) -> str:
+        return self._recommendation
+    
+    @recommendation.setter
+    def recommendation(self, recommendation: str):
+        if isinstance(recommendation, str):
+            self._recommendation = recommendation
+        else:
+            raise TypeError("Recommendation must be a string")
