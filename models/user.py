b62192d7b35a6a8390d82d21efd403fdbbf77cb3
Lucas dos Santos Santin <134106383+lucassantin@users.noreply.github.com>
2025-09-10 15:14:20 -0300

    feat<user>: classe to refer user

--- models/user.py
+++ models/user.py
@@ -1,3 +1,32 @@
 class User:
-    def __init__(self):
-        pass
+    def __init__(self, name: str, contact: str):
+        self.name = None
+        self.contact = None
+
+        def isinstance(name, str):
+            self.name = name
+
+        def isinstance(contact, str):
+            self.contact = contact
+
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @name.setter
+    def name(self, name: str):
+        if isinstance(name, str):
+            self._name = name
+        else:
+            raise TypeError("Name must be a string")
+        
+    @property
+    def contact(self) -> str:
+        return self._contact
+    
+    @contact.setter
+    def contact(self, contact: str):
+        if isinstance(contact, str):
+            self._contact = contact
+        else:
+            raise TypeError("Contact must be a string")
