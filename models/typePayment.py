0c931ff5e69cd80ba6c86fc0fa9bac257b5aeb57
Lucas dos Santos Santin <134106383+lucassantin@users.noreply.github.com>
2025-09-10 14:46:40 -0300

    typePayment class

---
+++ models/typePayment.py
@@ -0,0 +1,17 @@
+class TypePayment:
+    def __init__(self, name: str):
+        self.name = None
+
+        def isinstance(name, str):
+            self.name = name
+
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @name.setter
+    def name(self, name: str):
+        if isinstance(name, str):
+            self._name = name
+        else:
+            raise TypeError("Name must be a string")
