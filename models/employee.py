425449ffdbd96923bba2d8a52260bb68af854292
Augusto Roberto Tavares Júnior <eogutin@outlook.com>
2025-09-12 11:02:09 -0300

    feat<employee>: add Employee class

--- models/employee.py
+++ models/employee.py
@@ -1,3 +1,23 @@
-class Employee:
-    def __init__(self):
-        pass
+from user import User
+from especiality import Especiality
+
+class Employee(User):
+    def __init__(self, name: str, contact: str, especiality: Especiality):
+        super().__init__(name, contact)
+
+        self.__especiality = None
+
+        if isinstance(especiality, Especiality):
+            self.__especiality = especiality
+
+
+    @property
+    def especiality(self) -> Especiality:
+        return self.__especiality
+
+    @especiality.setter
+    def especiality(self, especiality: Especiality):
+        if isinstance(especiality, Especiality):
+            self.__especiality = especiality
+        else:
+            raise TypeError("Especiality must be an instance of Especiality")
