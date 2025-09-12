06599327c84f370c27141e6ea0c6f832d26be548
Augusto Roberto Tavares Júnior <eogutin@outlook.com>
2025-09-12 11:05:12 -0300

    feat<service>: add Service class

--- models/service.py
+++ models/service.py
@@ -1,3 +1,48 @@
 class Service:
-    def __init__(self):
-        pass
+    def __init__(self, name: str, description: str, price: float):
+        self.__name = None
+        self.__description = None
+        self.__price = None
+
+        if isinstance(name, str):
+            self.__name = name
+
+        if isinstance(description, str):
+            self.__description = description
+
+        if isinstance(price, (int, float)):
+            self.__price = float(price)
+
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        if isinstance(name, str):
+            self.__name = name
+        else:
+            raise TypeError("Name must be a string")
+
+    @property
+    def description(self) -> str:
+        return self.__description
+
+    @description.setter
+    def description(self, description: str):
+        if isinstance(description, str):
+            self.__description = description
+        else:
+            raise TypeError("Description must be a string")
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, price: float):
+        if isinstance(price, (int, float)):
+            self.__price = float(price)
+        else:
+            raise TypeError("Price must be a number")
