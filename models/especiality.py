f08132ab8223797a09df21380bc965d866ada49d
Augusto Roberto Tavares Júnior <eogutin@outlook.com>
2025-09-12 11:04:35 -0300

    feat<especiality>: fix the Speciality class name

--- models/especiality.py
+++
@@ -1,3 +0,0 @@
-class Especiality:
-    def __init__(self):
-        pass
---
+++ models/speciality.py
@@ -0,0 +1,33 @@
+class Speciality:
+    def __init__(self, name, description):
+        self.__name = None
+        self.__description = None
+
+        if isinstance(name, str):
+            self.__name = name
+
+        if isinstance(description, str):
+            self.__description = description
+
+
+    @property
+    def name(self) -> str:
+        return self.__name
+    
+    @name.setter
+    def name(self, name: str):
+        if isinstance(name, str):
+            self.__name = name
+        else:
+            raise TypeError("Name must be a string")
+        
+    @property
+    def description(self) -> str:
+        return self.__description
+    
+    @description.setter
+    def description(self, description: str):
+        if isinstance(description, str):
+            self.__description = description
+        else:
+            raise TypeError("Description must be a string")
