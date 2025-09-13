f7ef7b8fdc575af04373680b5ad42054c9126de2
Augusto Roberto Tavares Júnior <eogutin@outlook.com>
2025-09-13 10:55:56 -0300

    feat<table appointments>

---
+++ data/schema/appointment.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS appointments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    appointment_date DATETIME,
+    service_id INTEGER,
+    employee_id INTEGER,
+    payment_id INTEGER,
+    FOREIGN KEY (service_id) REFERENCES services(id),
+    FOREIGN KEY (employee_id) REFERENCES employees(id),
+    FOREIGN KEY (payment_id) REFERENCES payments(id)
+);
