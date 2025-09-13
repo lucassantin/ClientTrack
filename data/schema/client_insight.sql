e172137ecd15195a72c0816a565354c9e2036864
lucassantin <lucasdossantossantin@gmail.com>
2025-09-13 00:26:20 -0300

    feat<table client_insight>: table to refer multiples insights a client and your insight

---
+++ data/schema/client_insight.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS client_insight (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_id INTEGER NOT NULL,
+    insight_id INTEGER NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES clients(id),
+    FOREIGN KEY (insight_id) REFERENCES insights(id)
+)
