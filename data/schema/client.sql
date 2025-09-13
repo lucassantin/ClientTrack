6aaffd663f7c023094c236f61e84b6bb2f3b2065
lucassantin <lucasdossantossantin@gmail.com>
2025-09-13 00:23:34 -0300

    update<table client>: add accumulatedIndice

--- data/schema/client.sql
+++ data/schema/client.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS client (
     user_id INTEGER NOT NULL UNIQUE,
     birthday TEXT,
     insight_id INTEGER UNIQUE,
+    accumulatedIndice INTEGER DEFAULT 0,
     FOREIGN KEY (insight_id) REFERENCES insights(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 )
