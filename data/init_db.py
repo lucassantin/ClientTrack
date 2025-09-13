8a50acf53a7b5081e0924de796f0136b26a4f908
lucassantin <lucasdossantossantin@gmail.com>
2025-09-12 22:48:45 -0300

    update<database>: update struct to running script to gerenate database

--- data/init_db.py
+++ data/init_db.py
@@ -1,10 +1,28 @@
-import _sqlite3
+import sqlite3
+import os
 
-def init_db():
-    conn = _sqlite3.connect('clienttrack.db')
-    cursor = conn.cursor()
+def initialize_database():
+    DB_NAME = "clienttrack.db"
+    SCHEMA_DIR = "schema"
 
-    # Create tables
+    try:
+        with sqlite3.connect(DB_NAME) as conn:
+            cursor = conn.cursor()
+            print("Running the scripts of schema...")
 
-    conn.commit()
-    conn.close()
+            sql_files = [f for f in os.listdir(SCHEMA_DIR) if f.endswith('.sql')]
+            
+            for sql_file in sorted(sql_files):
+                filepath = os.path.join(SCHEMA_DIR, sql_file)
+                with open(filepath, 'r') as f:
+                    sql_script = f.read()
+                    cursor.executescript(sql_script) 
+                print(f" - Script '{sql_file}' executed.")
+
+            conn.commit()
+            print("Data base start with sucess.")
+    except sqlite3.Error as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    initialize_database()
