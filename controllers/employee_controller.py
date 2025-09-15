8420420eef782d6a19acf72908dc01965f47bdc2
Lucas dos Santos Santin <134106383+lucassantin@users.noreply.github.com>
2025-09-15 00:56:28 -0300

    feat(employee controller): code to manager table employees in db

--- controllers/employee_controller.py
+++ controllers/employee_controller.py
@@ -1,3 +1,22 @@
+import sqlite3
+from models.employee import Employee
+
+
+DB_NAME = "clienttrack.db"
 class EmployeeController:
-    def __init__(self):
-        pass
+    def add(self, employee: Employee):
+        sql = "insert into employees (user_id, specialty_id) values (?,?)"
+        data_tuple = (employee.user_id, employee.employee_id)
+
+        try:
+            with sqlite3.connect(DB_NAME) as conn:
+                cursor = conn.cursor()
+                cursor.execute(sql,data_tuple)
+                conn.commit()
+                print(f"Employee {employee.name} added successfully")
+                return True
+        except sqlite3.Error as e:
+            print(f"Error adding employee: {e}")
+            return False
+
+    def get_all(self): ...
