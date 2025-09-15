985f8c969c1c76c68073d2d862ed90472b7f917a
Lucas dos Santos Santin <134106383+lucassantin@users.noreply.github.com>
2025-09-15 00:57:25 -0300

    feat(insight controller): code to manager insight table in db

--- controllers/insight_controller.py
+++ controllers/insight_controller.py
@@ -1,3 +1,56 @@
+import sqlite3
+from models.insight import Insight
+
+DB_NAME = "clienttrack.db"
+
 class InsightController:
-    def __init__(self):
-        pass
+    def add(self, insight: Insight):
+        sql = "insert into insights (indice, recommendation) values (?,?)"
+        data_tuple = (insight.indice, insight.recommendation)
+
+        try:
+            with sqlite3.connect(DB_NAME) as conn:
+                cursor = conn.cursor()
+                cursor.execute(sql, data_tuple)
+                conn.commit()
+                print(f'Insight added successfully')
+                return cursor.lastrowid
+        except sqlite3.Error as e:
+            print(f"Error adding insight: {e}")
+            return False
+        
+    def get_all(self):
+        sql = "select id, indice, recommendation from insights"
+
+        try:
+            with sqlite3.connect(DB_NAME) as conn:
+                conn.row_factory = sqlite3.Row
+                cursor = conn.cursor()
+                cursor.execute(sql)
+
+                rows = cursor.fetchall()
+
+                insights = [Insight(id=row["id"], indice=row["indice"], recommendation=row["recommendation"]) for row in rows]
+
+                return insights
+        except sqlite3.Error as e:
+            print(f"Database error: {e}")
+            return []
+        
+    def get(self, indice:int, recommendation: str):
+        sql = 'select * from insights where indice = ? and recommendation = ?'
+
+        try:
+            with sqlite3.connect(DB_NAME) as conn:
+                conn.row_factory = sqlite3.Row
+                cursor = conn.cursor()
+                
+                row = cursor.execute(sql, (indice,recommendation)).fetchone()
+
+                if row:
+                    return Insight(id=row["id"], indice=row["indice"], recommendation=row["recommendation"])
+                else:
+                    return None
+        except sqlite3.Error as e:
+            print(f"Database error: {e}")
+            return None
