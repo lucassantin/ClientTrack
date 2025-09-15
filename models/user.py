37ad5204e3217e09ca2fe1ee8011a8b12fbbd489
Lucas dos Santos Santin <134106383+lucassantin@users.noreply.github.com>
2025-09-15 01:03:42 -0300

    update(user class): add id

--- models/user.py
+++ models/user.py
@@ -1,13 +1,26 @@
 class User:
-    def __init__(self, name: str, contact: str):
-        self.name = None
-        self.contact = None
+    def __init__(self, name: str, contact: str, user_id: int = None):
+        self._user_id = None
+        self._name = None
+        self._contact = None
+
+        if isinstance(user_id, int):
+            self._user_id = user_id
 
         if isinstance(name, str):
             self.name = name
 
         if isinstance(contact, str):
             self.contact = contact
+
+    @property
+    def user_id(self):
+        return self._user_id
+    
+    @user_id.setter
+    def user_id(self, user_id:int):
+        if isinstance(user_id, int):
+            self._user_id = user_id
 
     @property
     def name(self) -> str:
