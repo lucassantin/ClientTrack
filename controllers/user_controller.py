7f76bbb9f8a7a02287e926c67cf969a41fc40052
Lucas dos Santos Santin <134106383+lucassantin@users.noreply.github.com>
2025-09-15 00:57:55 -0300

    feat(user controller): code to manager user table in db

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,3 +1,21 @@
+import sqlite3
+from models.user import User
+
+DB_NAME = "clienttrack.db"
+
 class UserController:
-    def __init__(self):
-        pass
+    def add(self, user: User):
+        sql = "insert into users (name, contact) values (?,?)"
+        data_tuple = (user.name, user._contact)
+
+        try:
+            with sqlite3.connect(DB_NAME) as conn:
+                cursor = conn.cursor()
+                cursor.execute(sql, data_tuple)
+                conn.commit()
+                return cursor.lastrowid
+        except sqlite3.Error as e:
+            print(f"Error adding user {user.name}: {e}")
+
+    
+
