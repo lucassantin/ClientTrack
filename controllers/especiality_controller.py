9563f4bfe55dca080494bf467c82957dfb2304fe
Lucas dos Santos Santin <134106383+lucassantin@users.noreply.github.com>
2025-09-15 00:57:04 -0300

    feat(especialty controller): code to manager especialty table in db

--- controllers/especiality_controller.py
+++ controllers/especiality_controller.py
@@ -1,3 +1,27 @@
+import sqlite3
+from models.specialty import Specialty
+
+DB_NAME = "clienttrack.db"
+
 class EspecialityController:
-    def __init__(self):
-        pass
+    def add(self): ...
+
+    def get_all(self):
+        sql = "select id, name, description from specialties"
+
+        try:
+            with sqlite3.connect(DB_NAME) as conn:
+                conn.row_factory = sqlite3.Row
+                cursor = conn.cursor()
+                cursor.execute(sql)
+
+                rows = cursor.fetchall()
+
+                specialties = [Specialty(specialty_id=row["id"], name=row["name"], description=row["description"]) for row in rows]
+
+                return specialties
+        except sqlite3.Error as e:
+            print(f"Database error: {e}")
+            return []    
+        
+
