e34390fe98721123a7c40642aaf9a379e680332e
Lucas dos Santos Santin <134106383+lucassantin@users.noreply.github.com>
2025-09-15 00:55:12 -0300

    build(main): implementation struct to run the project in terminal

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
+from data.init_db import initialize_database
+from views.insight_view import insightView
+from views.authentication_view import employeeView, clientView
+
 if __name__ == "__main__":
-    print("Sistema iniciado com sucesso!")
+    initialize_database()
+    print()
+
+    print("System start with sucess!")
+    print()
+
+
 
 
     while True:
-        command = input("Digite um comando (ou 'Ctrl+C' para sair): ")
+        try:
+            #print("<-----Commands to manager insights----->")
+            #print()
+            #print("Type 1 to create a insight")
+            #print("Type 2 to return all insights")
+            #print()
+
+            print("<-----Commands to manager users----->")
+            print()
+            print("Type 3 to create a employee")
+            print("Type 4 to return all employees")
+            print("Type 5 to create a client")
+            print("Type 6 to return all clients")
+            print()
+            command = input("Type a command (or 'Ctrl+C' to close): ")
+            print()
+
+            match command:
+                case "1":   insightView().create()
+                case "2":   insightView().get_all()
+                case "3":   employeeView().create()
+                case "4":    ...
+                case "5":   clientView().create()
+                case "6":   clientView().get_all()
+                    
+
+        except KeyboardInterrupt:
+            print("System close")
+            break
