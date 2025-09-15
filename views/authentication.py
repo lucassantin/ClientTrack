df669a7163d026c0ef82d1bdd0f72f09493afc2f
Lucas dos Santos Santin <134106383+lucassantin@users.noreply.github.com>
2025-09-15 01:05:04 -0300

    delete(authentication): delete file with wrong name

--- views/authentication.py
+++
@@ -1,9 +0,0 @@
-from models.employee import Employee
-from models.specialty import Specialty
-
-class UsersView:
-    """View for user-related operations."""
-
-    def create_employee(self, name: str, contact: str, specialty: Specialty):
-        """Create a new employee."""
-        return Employee(name, contact, specialty)
