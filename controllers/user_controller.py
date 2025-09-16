103605262e37ddd5aaefc7eac998c8ca62cf8035
lucassantin <lucasdossantossantin@gmail.com>
2025-09-15 23:51:15 -0300

    fix(user controller): acess getter and correction of return

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -6,16 +6,15 @@ DB_NAME = "clienttrack.db"
 class UserController:
     def add(self, user: User):
         sql = "insert into users (name, contact) values (?,?)"
-        data_tuple = (user.name, user._contact)
+        data_tuple = (user.name, user.contact)
 
         try:
             with sqlite3.connect(DB_NAME) as conn:
                 cursor = conn.cursor()
                 cursor.execute(sql, data_tuple)
                 conn.commit()
-                return cursor.lastrowid
+                return cursor.lastrowid, True
         except sqlite3.Error as e:
-            print(f"Error adding user {user.name}: {e}")
-
+            return False, e
     
 
